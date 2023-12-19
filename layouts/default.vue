5b71135a55e2bb1e7fd16c5a72506e891ad89059
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2023-12-19 10:23:26 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="px-4 md:px-[0px]">
-    <Header />
-    <div class="container mx-auto">
-      <slot />
+    <div class="flex justify-center items-center mx-auto h-screen">
+      <div class="flex flex-col text-center justify-center w-6/12 p-8">
+        <img
+          src="../public/assets/images/cat-sleep.svg"
+          class="w-[300px] mx-auto mb-6"
+          alt=""
+        />
+        <h2 class="text-3xl font-medium mb-4">Website is Under Construction</h2>
+        <p class="text-base font-normal">
+          Website under construction for improvements. Apologies for any
+          inconvenience. We'll be back soon. Thanks for your understanding!
+        </p>
+      </div>
     </div>
-    <Footer />
   </div>
 </template>
 
